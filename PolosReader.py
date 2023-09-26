d2209f53fb170e8d7cca6c89e20c3d836c863e36
Alexander.Chad <Alexander.Chad@yandex.ru>
2023-09-26 23:47:53 +0700

    add comments, fix bugs

--- PolosReader.py
+++ PolosReader.py
@@ -6,102 +6,122 @@ from tkinter import filedialog
 import numpy as np
 import math
 
-img: np.array
-display_img: np.array
-img_label: tk.Label
-lines_c = []  # lines
-adj_threshold: tk.Scale
+img: np.array  # загруженное изображение
+display_img: np.array  # выводимое на дисплей изображение
+img_label: tk.Label  # объект, на который выводится изображение
+lines_c = []  # координаты центров линий по оси X
+
+adj_threshold: tk.Scale  # объект ползунка threshold (порог для маски)
+# объект ползунка dopusk (смешивание соседних полос в линии)
 adj_dopusk: tk.Scale
-adj_overlap: tk.Scale
+adj_overlap: tk.Scale  # объект ползунка overlap (перекрытие)
+# объект ползунка gain_bg (усиление фона, т.е. вне полос)
 adj_gain_bg: tk.Scale
-adj_gain_line: tk.Scale
-adj_brightness: tk.Scale
-adj_contrast: tk.Scale
+adj_gain_line: tk.Scale  # объект ползунка gain_line (усиление линий, в белый)
+adj_brightness: tk.Scale  # объект ползунка brightness (яркость)
+adj_contrast: tk.Scale  # объект ползунка contrast (контрастность)
 
-height = 0
-width = 0
+height = 0  # высота загрженного изображения
+width = 0  # ширина загрженного изображения
+line_width = 0  # средняя ширина линий
 
-first_int = True
+first_int = True  # флаг первого запуска. Добавляет задержку между этапами
 
-root = tk.Tk()
+root = tk.Tk()  # используем графический интерфейс Tkinter
 root.wm_title("ImgMagic")
 
 
-def UpdImage(Show_img):
+def UpdImage(Show_img, BC=True):  # Вывод изображения на экран
     global img_label, adj_brightness, adj_contrast, display_img
-    img_upgraded = apply_brightness_contrast(
-        Show_img, adj_brightness.get(), adj_contrast.get())
-    converted_img = Image.fromarray(img_upgraded)
-    converted_img.thumbnail(size=(809, 688))
-    imgtk = ImageTk.PhotoImage(image=converted_img)
-    img_label.imgtk = imgtk
-    img_label.configure(image=imgtk)
+    if BC:  # если необходимо применить коррекцию яркости и контрастности
+        img_upgraded = apply_brightness_contrast(
+            Show_img, adj_brightness.get(), adj_contrast.get())  # применяем настройки яркости и контрастности
+    else:
+        img_upgraded = np.copy(Show_img)
+    converted_img = Image.fromarray(img_upgraded)  # конвертируем в формат PIL
+    converted_img.thumbnail(size=(809, 688))  # подгоняем размер
+    imgtk = ImageTk.PhotoImage(image=converted_img)  # конвертируем в формат TK
+    img_label.imgtk = imgtk  # присваиваем
+    img_label.configure(image=imgtk)  # обновляем
     print('Img updated')
-    root.update()
+    root.update()  # обновляем UI
+    # сохраняем в буфер для возможности позднее сохранить в файл
     display_img = np.copy(img_upgraded)
-    if first_int:
-        time.sleep(1)
+    if first_int:  # если первый проход
+        time.sleep(3)  # между обновлениями делаем задержку
 
 
-def FindLines():
-    global lines_c
+def FindLines():  # Ищем линии
+    global lines_c, line_width
+    # Обнуляем
     lines_c = []
     contours = []
-    # setting threshold of gray image
+    lines_w = []
+    # выделяем по порогу маску
     _, threshold = cv2.threshold(
         img, adj_threshold.get(), 255, cv2.THRESH_BINARY)
-    # display(Image.fromarray(cv2.cvtColor(threshold, cv2.COLOR_GRAY2RGB)))
+    # обновляем изображение
     UpdImage(cv2.cvtColor(threshold, cv2.COLOR_GRAY2RGB))
 
-    # using a findContours() function
+    # получаем контуры палочек
     contours, _ = cv2.findContours(
         threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    # конвертируем в пространство RGB из тонов серого
     img2 = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
-    # here we are ignoring first counter because
-    # findcontour function detects whole image as shape
+    # игнорием первый контур, он для нас побочный
     contours = contours[1:]
 
-    # list for storing names of shapes
+    # перебираем контуры
     for contour in contours:
-        # using drawContours() function
+        # рисуем найденный контур на изображении
         cv2.drawContours(img2, [contour], 0, (255, 0, 0), 2)
 
-        # compute the center of the contour
+        # вычисляем с помощью моментов центр контура
         M = cv2.moments(contour)
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
         '''
+        # старый метод нахождения краев линии
         rect = cv2.minAreaRect(contour)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         cv2.drawContours(img2, [box], 0, (0, 0, 255), 2)
         x, y, w, h = cv2.boundingRect(box)
         '''
-        xl=[]
-        xr=[]
+        # новый метод нахождения краев линии
+        xl = []
+        xr = []
         # получаем средний x и длину отрезка
-        con_p=[[(contour[i-1][0][0]+contour[i][0][0])/2,math.dist(contour[i-1][0],contour[i][0])] for i in range(len(contour))]
-        for cnp in con_p:
-            if cnp[0]<cX:
+        con_p = [[(contour[i-1][0][0]+contour[i][0][0])/2,
+                  math.dist(contour[i-1][0], contour[i][0])] for i in range(len(contour))]
+        for cnp in con_p:  # перебираем отрезки
+            if cnp[0] < cX:  # если левый, то записываем в левый
                 xl.append(cnp)
-            elif cnp[0]>cX:
+            elif cnp[0] > cX:  # если правый, то записываем в правый
                 xr.append(cnp)
-        xlc=sum(x[0]*x[1] for x in xl)/sum(x[1] for x in xl)
-        xrc=sum(x[0]*x[1] for x in xr)/sum(x[1] for x in xr)
-        w=round(xrc-xlc)
+        # вычисляем средний X, определяя за вес длину отрезка
+        xlc = sum(x[0]*x[1] for x in xl)/sum(x[1] for x in xl)
+        xrc = sum(x[0]*x[1] for x in xr)/sum(x[1] for x in xr)
+        # вычисляем ширину линии
+        w = xrc-xlc
 
-
-        # [center, 1/2 line width]
-        lines_c.append([cX, round(w/2)])
+        # записываем вычисленные параметры контура (линии)
+        lines_c.append(cX)
+        lines_w.append(w)
+        # рисуем линию
         cv2.line(img2, (cX, 0), (cX, height),
                  (0, 255, 0), thickness=2)
-        # print(f'x_center={x_center}, y_min={y_min}, y_max={y_max}.')
-    lines_c.sort(key=lambda x: x[0])
-    UpdImage(img2)
+    # сортируем линии по координатам
+    lines_c.sort()
+    # вычисляем среднюю ширину линии
+    line_width = round(sum(lines_w)/len(lines_w))
+    # обновляем изображение
+    UpdImage(img2, False)
 
 
+# коррекция яркости и контрастности изображения
 def apply_brightness_contrast(input_img, brightness=0, contrast=0):
     if brightness != 0:
         if brightness > 0:
@@ -123,93 +143,99 @@ def apply_brightness_contrast(input_img, brightness=0, contrast=0):
     return buf
 
 
-def fill_line(img_f: np.array, lx: int, rx: int, y: int, color: int):
-    for i in range(lx, rx):
-        img_f[i, y] = color
-
-
-def RestoreImg():
-    global lines_c
+def RestoreImg():  # восстанавливаем изображение
+    global lines_c, line_width
+    # инвертируем изображение
     img_res = 255-np.copy(img)
     # получаем половину расстояния между соседними линиями
-    dist_line = round((lines_c[-1][0]-lines_c[0][0])/(len(lines_c)-1)/2)
+    dist_line = round((lines_c[-1]-lines_c[0])/(len(lines_c)-1)/2)
     # получаем среднюю половину ширины линии
-    width_line = round(sum([lines_c[i][1]
-                       for i in range(len(lines_c))])/len(lines_c))
+    width_line = round(line_width/2)
+    # считываем ползунки
     gain_bg = adj_gain_bg.get()/100
     gain_line = adj_gain_line.get()/100
     dopusk = adj_dopusk.get()/100
     overlap = adj_overlap.get()/100
+    # считаем ширину перекрытия
     overlap_width = round(overlap*dist_line)
+    # цвет прошлого прохода
     prev_line_right_color = 0
+    # перебираем линии
     for line in lines_c:
-        # for left
-        llp = line[0]-dist_line
-        lrp = line[0]-width_line
-        # for right
-        rlp = line[0]+width_line
-        rrp = line[0]+dist_line
+        # для левой стороны линии
+        llp = line-dist_line  # левая граница левого сектора
+        lrp = line-width_line  # правая граница левого сектора
+        # для правой стороны линии
+        rlp = line+width_line  # левая граница правого сектора
+        rrp = line+dist_line  # правая граница правого сектора
+        # проходимся по высоте
         for j in range(height):
-            # for left
-            if llp >= 0:
+            # для левой стороны линии
+            if llp >= 0:  # проверяем что есть место для работы
                 color_l = round(sum([img_res[j, i]
                                      for i in range(llp, lrp)])/(lrp-llp))
             else:
                 continue
-            # for right
-            if rrp <= width:
+            # для правой стороны линии
+            if rrp <= width:  # проверяем что есть место для работы
                 color_r = round(sum([img_res[j, i]
                                      for i in range(rlp, rrp)])/(rrp-rlp))
             else:
                 continue
+            # считаем средний цвет пары секторов (левый и правый)
             delta_color = (color_l-color_r)/2
+            # обновляем основной цвет для левого
             color_l = round(color_l-delta_color*dopusk)
+            # обновляем основной цвет для правого
             color_r = round(color_r+delta_color*dopusk)
-            # fill_line left
+            # восстанавливаем левый сектор
             for i in range(llp, lrp):
                 img_res[j, i] = round(
                     img_res[j, i]*gain_bg+color_l*(1-gain_bg))
-            # fill_line right
+            # восстанавливаем правый сектор
             for i in range(rlp, rrp):
                 img_res[j, i] = round(
                     img_res[j, i]*gain_bg+color_r*(1-gain_bg))
-            # fill_line center
+            # считаем основной цвет на основании крайних пикселей
             color_c = round((img_res[j, lrp]+img_res[j, rlp])/2)
+            # восстанавливаем центральный сектор (область линии)
             for i in range(lrp, rlp):
                 img_res[j, i] = round(color_c*(1-gain_line))
-
+            # применяем перекрытие в месте стыковки секторов линий
             for i in range(llp-overlap_width, llp+overlap_width):
                 img_res[j, i] = round((prev_line_right_color+color_l)/2)
-
+            # запоминаем цвет линии
             prev_line_right_color = color_r
-
     print('Success')
+    # обновляем изображение (не забываем инвертировать обратно)
     UpdImage(cv2.cvtColor(255-img_res, cv2.COLOR_GRAY2RGB))
-    # for right
 
 
-def ApplyProp():
-    UpdImage(cv2.cvtColor(img, cv2.COLOR_GRAY2RGB))
+def ApplyProp():  # Применяем настройки и перестраиваем изображение
+    UpdImage(cv2.cvtColor(img, cv2.COLOR_GRAY2RGB), False)
     FindLines()
     RestoreImg()
 
 
-def SaveImg():
+def SaveImg():  # Сохраняем изображение
     global display_img
     img_name = time.strftime("%d-%m-%Y_%H.%M.%S", time.localtime()) + \
-        f'_t{adj_threshold.get()},_d{adj_dopusk.get()},_o{adj_overlap.get()},_gbg{adj_gain_bg.get()},_gl{adj_gain_line.get()},_b{adj_brightness.get()},_c{adj_contrast.get()}.png'
+        f'_t{adj_threshold.get()},_d{adj_dopusk.get()},_o{adj_overlap.get()},_gbg' + \
+        f'{adj_gain_bg.get()},_gl{adj_gain_line.get()},_b{adj_brightness.get()},_c{adj_contrast.get()}.png'
     cv2.imwrite(img_name, display_img)
 
 
 if __name__ == "__main__":
+    # вызываем диалог открытия изображения
     img_path = filedialog.askopenfilename(filetypes=(
         ("Image files", "*.png;*.jpg;*.jpeg;*.bmp"), ("All files", "*.*")))
 
-    # 'scale_2400.png'
+    # читаем изображение
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    # читаем его размеры
     height, width = img.shape
 
-    # img_frame = tk.Frame(root, width=width, height=height, bg='gray').grid(row=0, column=0)
+    # прописываем UI Tk
     img_label = tk.Label(root, bg='gray')
     img_label.grid(row=0, column=0, columnspan=2)
 
@@ -232,49 +258,49 @@ if __name__ == "__main__":
     adj_dopusk.grid(row=3, column=1)
 
     label_adj_overlap = tk.Label(root, text="adj_overlap, %")
-    label_adj_overlap.grid(row=3, column=0)
+    label_adj_overlap.grid(row=4, column=0)
     adj_overlap = tk.Scale(root, length=250, from_=0,
                            to=100, orient=tk.HORIZONTAL)
-    adj_overlap.grid(row=3, column=1)
+    adj_overlap.grid(row=4, column=1)
 
     label_adj_gain_bg = tk.Label(root, text="adj_gain_bg, %")
-    label_adj_gain_bg.grid(row=4, column=0)
+    label_adj_gain_bg.grid(row=5, column=0)
     adj_gain_bg = tk.Scale(root, length=250, from_=0,
                            to=100, orient=tk.HORIZONTAL)
-    adj_gain_bg.grid(row=4, column=1)
+    adj_gain_bg.grid(row=5, column=1)
 
     label_adj_gain_line = tk.Label(root, text="adj_gain_line, %")
-    label_adj_gain_line.grid(row=5, column=0)
+    label_adj_gain_line.grid(row=6, column=0)
     adj_gain_line = tk.Scale(root, length=250, from_=0,
                              to=100, orient=tk.HORIZONTAL)
-    adj_gain_line.grid(row=5, column=1)
+    adj_gain_line.grid(row=6, column=1)
 
     label_adj_brightness = tk.Label(root, text="adj_brightness")
-    label_adj_brightness.grid(row=6, column=0)
+    label_adj_brightness.grid(row=7, column=0)
     adj_brightness = tk.Scale(
         root, length=250, from_=-127, to=127, orient=tk.HORIZONTAL)
-    adj_brightness.grid(row=6, column=1)
+    adj_brightness.grid(row=7, column=1)
 
     label_adj_contrast = tk.Label(root, text="adj_contrast")
-    label_adj_contrast.grid(row=7, column=0)
+    label_adj_contrast.grid(row=8, column=0)
     adj_contrast = tk.Scale(root, length=250, from_=-
                             127, to=127, orient=tk.HORIZONTAL)
-    adj_contrast.grid(row=7, column=1)
+    adj_contrast.grid(row=8, column=1)
 
-    # def settings
+    # настройки по-умолчанию
     adj_threshold.set(127)
-    adj_dopusk.set(50)
-    adj_overlap.set(50)
+    adj_dopusk.set(0)
+    adj_overlap.set(0)
     adj_gain_bg.set(50)
     adj_gain_line.set(50)
     adj_brightness.set(0)
     adj_contrast.set(0)
 
+    # запускаем перестроение
     ApplyProp()
 
+    # отмечаем, что один раз уже строили
     first_int = False
 
+    # отдаем управление UI
     root.mainloop()
-
-    # img_out = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
-    # cv2.imwrite('.\\' + name + '_DQRC.png', img)
